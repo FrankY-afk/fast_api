131e83267a2a915c4a90b5ebd17fe983d1e3a5da
Chirag Madan <chiragmadan1680@gmail.com>
2026-08-01 19:46:23 +0530

    post

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Path, HTTPException, Query
 import json
+from pydantic import BaseModel,Field,computed_field
+from typing import Annotated,Literal
 
 app = FastAPI()
 
+class patient(BaseModel):
+    id:Annotated[str,Field(...,description='id of the patient',examples=['p001'])]
+    name:Annotated[str,Field(...,description='name of the patient',examples=['john'])]
+    city:Annotated[str,Field(...,description='name of the city')]
+    age:Annotated[int,Field(...,gt=0,lt=120,description='what is the age of patient')]
+    gender:Annotated[Literal['male','female','others'],Field(...,description='gender of the patient')]
+    height:Annotated[float,Field(...,gt=0,description='height of the ')]
+    weight:Annotated[float,Field(...,gt=0,description='weight of the pattetnt')]
+
+    @computed_field
+    @property
+    def bmi(self) ->float:
+        bmi=self.weight/self.height**2
+        return bmi
+    @computed_field
+    @property
+    def verdict(self)->float:
+        if(self.bmi<18.5):
+            return 'underweight'
+        elif(self.bmi <30):
+            return 'normal'
+        else:
+            return 'obese'
 def load_data():
     with open('patients.json', 'r') as f:
         ans = json.load(f)
     return ans
+def save_data():
+    with open('patients.json', 'w') as f:
+        json.dump(f)
+
 
 
 @app.get("/")
@@ -65,3 +95,20 @@ def sort_patients(
     )
 
     return sorted_data
+
+@app.post('/create')
+def create_patient(patient:patient):
+    # load data
+    data=load_data()
+    # check if it is also present
+    if(patient.id in data):
+        raise HTTPException(status_code=400,detail='patient alr present')
+    # create new
+    data['new_patient_id']=patient.model_dump(exclude='id')
+    # converting pydantic data to python
+    save_data()
+    # saved data
+    return JSONResponse(
+    status_code=201,
+    content={"message": "Patient created successfully"}
+)
