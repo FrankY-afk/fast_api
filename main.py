8c7df47df6446d4239ab3e7ab84ec0b2af82a052
Chirag Madan <chiragmadan1680@gmail.com>
2026-08-03 10:09:50 +0530

    final changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Path, HTTPException, Query
 import json
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal,Optional 
 
 app = FastAPI()
 
@@ -124,3 +124,30 @@ def create_patient(patient: patient):
         status_code=201,
         content={"message": "Patient created successfully"}
     )
+class Update(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None)]
+    gender: Annotated[Optional[Literal["male", "female", "other"]],Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None)]
+    weight: Annotated[Optional[float], Field(default=None)]
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id:str,patient_update:Update):
+    data=load_data()
+    if patient_id not in data:
+        HTTPException(status_code=404,detail='patient id is wrong')
+    existinfo=data[patient_id] 
+    updated_patient_info=patient_update.model_dump(exclude_unset=True)
+
+    for key,value in updated_patient_info.items():
+        existinfo[key]=value
+
+    existinfo['id']=patient_id
+    patient_py_obj=patient(**existinfo)
+    existinfo=patient_py_obj.model_dump(exclude='id')
+    data[patient_id]=existinfo
+
+    save_data(data) 
+
+
